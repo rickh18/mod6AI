48f042413fd406dd0f009c713dda10d0ce3b85ab
Magnificus36 <magnificus@outlook.com>
2014-12-18 00:33:57 +0100

    More performance data!

--- src/mod6AI/controller/Simple.java
+++ src/mod6AI/controller/Simple.java
@@ -12,9 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -171,6 +169,7 @@ public class Simple {
 
         Map<ClassificationType, Long> classifiedC1TestData;
         Map<ClassificationType, Long> classifiedC2TestData;
+        HashSet<ConfusionMatrix> confusionMatrices = new HashSet<>();
 
         double[] ks = {0.1, 0.25, 0.5, 0.75, 0.9, 1, 1.1, 1.25, 1.5, 1.75, 2, 2.5, 3, 5};
         int[] thresholds = {0, 2, 5, 10, 25, 50, 75, 100, 150};
@@ -192,15 +191,84 @@ public class Simple {
                         .collect(Collectors.groupingBy(o -> o, Collectors.counting()));
 
                 ConfusionMatrix confusionMatrix = new ConfusionMatrix(
-                        classifiedC1TestData.get(ClassificationType.C1),
-                        classifiedC1TestData.get(ClassificationType.C2),
-                        classifiedC2TestData.get(ClassificationType.C1),
-                        classifiedC2TestData.get(ClassificationType.C2));
+                        classifiedC1TestData.getOrDefault(ClassificationType.C1, 0L),
+                        classifiedC1TestData.getOrDefault(ClassificationType.C2, 0L),
+                        classifiedC2TestData.getOrDefault(ClassificationType.C1, 0L),
+                        classifiedC2TestData.getOrDefault(ClassificationType.C2, 0L),
+                        String.format("k: %1.2f; threshold: %d", k, threshold));
+
+                confusionMatrices.add(confusionMatrix);
 
                 System.out.println(confusionMatrix);
                 System.out.println("--------------------------------------------");
             }
         }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("********************************************");
+        System.out.println("Best and worst overview:");
+        System.out.println();
+
+        List<ConfusionMatrix> topAccuracy = confusionMatrices.stream().sorted((cm1, cm2) -> Double.compare(cm1.accuracy(), cm2.accuracy())).collect(Collectors.toList());
+        List<ConfusionMatrix> topErrorRate = confusionMatrices.stream().sorted((cm1, cm2) -> Double.compare(cm1.errorRate(), cm2.errorRate())).collect(Collectors.toList());
+        List<ConfusionMatrix> topRecallC1 = confusionMatrices.stream().sorted((cm1, cm2) -> Double.compare(cm1.recallC1(), cm2.recallC1())).collect(Collectors.toList());
+        List<ConfusionMatrix> topRecallC2 = confusionMatrices.stream().sorted((cm1, cm2) -> Double.compare(cm1.recallC2(), cm2.recallC2())).collect(Collectors.toList());
+        List<ConfusionMatrix> topPrecisionC1 = confusionMatrices.stream().sorted((cm1, cm2) -> Double.compare(cm1.precisionC1(), cm2.precisionC1())).collect(Collectors.toList());
+        List<ConfusionMatrix> topPrecisionC2 = confusionMatrices.stream().sorted((cm1, cm2) -> Double.compare(cm1.precisionC2(), cm2.precisionC2())).collect(Collectors.toList());
+
+        System.out.println("*** Accuracy ***");
+        System.out.println(" * lowest *");
+        System.out.println(topAccuracy.get(0));
+        System.out.println();
+        System.out.println(" * highest *");
+        System.out.println(topAccuracy.get(topAccuracy.size() -1));
+        System.out.println();
+        System.out.println();
+
+        System.out.println("*** Error Rate ***");
+        System.out.println(" * lowest *");
+        System.out.println(topErrorRate.get(0));
+        System.out.println();
+        System.out.println(" * highest *");
+        System.out.println(topErrorRate.get(topErrorRate.size() -1));
+        System.out.println();
+        System.out.println();
+
+        System.out.println(String.format("*** Recall %s ***", ClassificationType.C1));
+        System.out.println(" * lowest *");
+        System.out.println(topRecallC1.get(0));
+        System.out.println();
+        System.out.println(" * highest *");
+        System.out.println(topRecallC1.get(topRecallC1.size() -1));
+        System.out.println();
+        System.out.println();
+
+        System.out.println(String.format("*** Recall %s ***", ClassificationType.C2));
+        System.out.println(" * lowest *");
+        System.out.println(topRecallC2.get(0));
+        System.out.println();
+        System.out.println(" * highest *");
+        System.out.println(topRecallC2.get(topRecallC2.size() -1));
+        System.out.println();
+        System.out.println();
+
+        System.out.println(String.format("*** Precision %s ***", ClassificationType.C1));
+        System.out.println(" * lowest *");
+        System.out.println(topPrecisionC1.get(0));
+        System.out.println();
+        System.out.println(" * highest *");
+        System.out.println(topPrecisionC1.get(topPrecisionC1.size() -1));
+        System.out.println();
+        System.out.println();
+
+        System.out.println(String.format("*** Precision %s ***", ClassificationType.C2));
+        System.out.println(" * lowest *");
+        System.out.println(topPrecisionC2.get(0));
+        System.out.println();
+        System.out.println(" * highest *");
+        System.out.println(topPrecisionC2.get(topPrecisionC2.size() -1));
     }
 
     public static void main(String[] args) {
@@ -215,38 +283,38 @@ public class Simple {
 //        } catch (FileNotFoundException e) {
 //            e.printStackTrace();
 //        }
-//        try {
-//            testPerformance();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        AI ai = trainedAI(1,0);
-        System.out.print("Where to save: ");
-        String file = in.nextLine();
         try {
-            ai.save(file);
+            testPerformance();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        AI ai2 = new AI(1);
-        boolean success = false;
-        try {
-            success = ai2.load(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (success) {
-            System.out.println(ai.classify("Test"));
-            System.out.println(ai2.classify("Test"));
 
-            if (ai.getTotalNumberOfWordsByType(ClassificationType.C1) == ai2.getTotalNumberOfWordsByType(ClassificationType.C1)
-                    && ai.getTotalNumberOfWordsByType(ClassificationType.C2) == ai2.getTotalNumberOfWordsByType(ClassificationType.C2)) {
-                System.out.println(":)");
-            } else {
-                System.out.println(":(");
-            }
-        }
+//        AI ai = trainedAI(1,0);
+//        System.out.print("Where to save: ");
+//        String file = in.nextLine();
+//        try {
+//            ai.save(file);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        AI ai2 = new AI(1);
+//        boolean success = false;
+//        try {
+//            success = ai2.load(file);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        if (success) {
+//            System.out.println(ai.classify("Test"));
+//            System.out.println(ai2.classify("Test"));
+//
+//            if (ai.getTotalNumberOfWordsByType(ClassificationType.C1) == ai2.getTotalNumberOfWordsByType(ClassificationType.C1)
+//                    && ai.getTotalNumberOfWordsByType(ClassificationType.C2) == ai2.getTotalNumberOfWordsByType(ClassificationType.C2)) {
+//                System.out.println(":)");
+//            } else {
+//                System.out.println(":(");
+//            }
+//        }
     }
 
 }
